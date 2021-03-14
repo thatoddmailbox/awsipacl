ea1c51550eabcfc8459bee25effeb2e833746d6e
Alex Studer <alex@studer.dev>
2021-03-13 23:23:39 -0500

    frontend: can POST to the login route

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,14 @@
-alert("wow");
+window.addEventListener("load", function() {
+	document.getElementById("test").addEventListener("click", function() {
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "/login");
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				var response = xhr.responseText;
+				console.log(response);
+				console.log(JSON.parse(response));
+			}
+		};
+		xhr.send();
+	});
+});
